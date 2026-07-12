7781febd1d021617877a6036c092618e79d079a3
Krishna Kushwaha <kushwaha.krishna1233@gmail.com>
2026-07-12 22:45:16 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+export const FIREBASE_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123:web:abc"
+};
